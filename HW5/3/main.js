1e0aa56f162afc35d0e808a5db52918bc357e064
Nadezhda Khmelnikova <nkhmelnikova@gmail.com>
2017-04-07 09:12:04 +0300

    HW5 task3

---
+++ HW5/3/main.js
@@ -0,0 +1,25 @@
+var names = ["Алексей", "Надежда", "Сергей", "Мария", "Александра", "Валерия"];
+var cities = ["Минск", "Гродно", "Гомель", "Витебск", "Могилев", "Брест"];
+var goods = ['Книга "Мастер и Маргарита"', 'Подсвечник "Звезда"', 'Горшок для цветов "Беларусь"', 'Зеркало "Сияние"', 'Кружка "Универсальная"', 'Ковер "Модерн"']
+var images = ["book.jpeg", "star.jpg", "FlowerPot.jpg", "mirror.jpg", "cup.jpg", "carpet.jpg"]
+
+function random(numb) {
+    return Math.floor(Math.random()*100%numb);
+}
+var body = document.getElementById("body");
+var num = 0, numGoods = 0;
+var I = setInterval(function(){
+    num = random(6);
+    var div = document.createElement("div");
+    body.appendChild(div);
+    var img  = document.createElement("img");
+    div.appendChild(img);
+    img.setAttribute("src", "img/" + images[num]);
+    var p = document.createElement("span");
+    div.appendChild(p);
+    p.innerText = names[random(6)] + " из г." + cities[random(6)] + " купил " + random(10) + " ед. товара " + goods[num];
+    numGoods++;
+    if (numGoods > 9) {
+        clearInterval(I);
+    }
+},3000);
